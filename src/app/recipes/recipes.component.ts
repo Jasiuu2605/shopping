8f4ac82fc25e42da65ae6de7b5aca15e7d0ae8be
Jan Bańczerowski <jasiuu2605@gmail.com>
2023-07-12 20:29:10 +0200

    replacing event emiiters with Subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,15 +9,11 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService],
 })
 export class RecipesComponent {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
+
+  constructor() {}
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => { 
-        this.selectedRecipe = recipe;
-      }
-    );
+
   }
 }
